5785b76722ab15a02f82c9ae779875f7083354d4
PranavKrishna1503 <113581038+PranavKrishna1503@users.noreply.github.com>
2023-03-03 12:31:15 +0530

    Add files via upload

---
+++ day4/src/main/java/com/example/demo/ApiService.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+	@Autowired
+	StudentRepo repository;
+	public Optional<Student>getStudent(int id){
+		return repository.findById(id);
+	}
+	public String updateDetails(Student stu) {
+		repository.save(stu);
+		return "updated";
+	}
+	public String deleteDetails(int id)
+	{
+		repository.deleteById(id);
+		return "Id deleted";
+	}
+
+}
